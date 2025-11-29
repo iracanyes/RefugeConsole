671c7c6319e7723945af618a28c3acf71e717133
Iracanye S. <iracanyes@gmail.com>
2025-11-29 03:01:05 +0100

    Init projet : Configuration Docker container - Postgresql container

---
+++ RefugeConsole/ClassesMetiers/Model/Entities/ContactInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefugeConsole.ClassesMetiers.Model.Entities
+{
+    internal class ContactInfo
+    {
+    }
+}
---
+++ RefugeConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace RefugeConsole
+{
+    internal class Program
+    {
+        private static readonly ILogger MyLogger = LoggerFactory.Create( builder => builder.AddConsole()).CreateLogger<Program>();
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\tBienvenue au refuge\t\n================================");
+        }
+    }
+}
